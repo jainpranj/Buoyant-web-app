dfa330403d2504b9d198bc51cfa16d9166ec5c34
Pranjal Jain <jainpranj@gmail.com>
2017-05-15 23:30:47 -0400

    exception handling

--- web/app.py
+++ web/app.py
@@ -5,50 +5,59 @@ app = Flask(__name__)
 
 @app.route('/health')
 def health():
-    data = {'linkerd':'down','namerd':'down','namerd':'down','linkerd-tcp':'down','linkerd-viz':'down'}
+    
 	# linkerd health
-    r = requests.get('http://localhost:9990/admin/ping')
-    r3 = requests.get('http://localhost:9991/admin/ping')
-    r1 = requests.get('http://localhost:9992/metrics')
-    r2= requests.get('http://localhost:3000')
-    if r.text=="pong":
+	data = {'linkerd':'down','namerd':'down','namerd':'down','linkerd-tcp':'down','linkerd-viz':'down'}
+	try:
 
-		data['linkerd'] = 'up'
+		
+	    r = requests.get('http://localhost:9990/admin/ping')
+	    r3 = requests.get('http://localhost:9991/admin/ping')
+	    r1 = requests.get('http://localhost:9992/metrics')
+	    r2= requests.get('http://localhost:3000')
+	    if r.text=="pong":
 
-	# namerd health
-	
-    if r3.text=="pong":
+			data['linkerd'] = 'up'
 
-		data['namerd'] = 'up'
+		# namerd health
+		
+	    if r3.text=="pong":
 
-	# linkerd-tcp
+			data['namerd'] = 'up'
 
-	
-    if r1.status_code==200:
+		# linkerd-tcp
 
-		data['linkerd-tcp'] = 'up'
+		
+	    if r1.status_code==200:
 
-	# linkerd-viz
-	
-    if r2.status_code==200:
+			data['linkerd-tcp'] = 'up'
 
-		data['linkerd-viz'] = 'up'
+		# linkerd-viz
+		
+	    if r2.status_code==200:
 
-		response = app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
-    return response
+			data['linkerd-viz'] = 'up'
+
+			response = app.response_class(response=json.dumps(data),status=200,mimetype='application/json')
+			return response
+	except requests.exceptions.RequestException:
+		return "the linkerd application is not running"
+    
 
 @app.route('/shift',methods = ['GET'])
 def balance_traffic():
-
-	N = int(request.args.get('N'))
-	headers={}
-	path="http://localhost:4180/api/1/dtabs/default"
-	balancing=str(100-N)+"*/svc/redis1 & "+str(N)+"*/svc/redis2"
-	params=[{"prefix":"/svc","dst":"/#/io.l5d.fs"},{"prefix":"/svc/redis","dst":balancing}]
-	headers["Content-Type"] = "application/json"
-	response = requests.put(path,data=json.dumps(params),headers=headers)
-	print(response.status_code)
-	return str(response.status_code)	
+	try:
+		N = int(request.args.get('N'))
+		headers={}
+		path="http://localhost:4180/api/1/dtabs/default"
+		balancing=str(100-N)+"*/svc/redis1 & "+str(N)+"*/svc/redis2"
+		params=[{"prefix":"/svc","dst":"/#/io.l5d.fs"},{"prefix":"/svc/redis","dst":balancing}]
+		headers["Content-Type"] = "application/json"
+		response = requests.put(path,data=json.dumps(params),headers=headers)
+		print(response.status_code)
+		return str(response.status_code)
+	except requests.exceptions.RequestException:
+		return "the linkerd application is not running"	
 
 
 @app.route('/',methods = ['GET'])
